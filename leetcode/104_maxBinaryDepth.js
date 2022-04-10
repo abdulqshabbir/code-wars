c4bd232b41422fdad1a5e3f0654d836758d2b7d5
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-04-11 07:54:33 +0800

    completed iterative BFS approach for maxBinaryDepth

--- leetcode/104_maxBinaryDepth.js
+++ leetcode/104_maxBinaryDepth.js
@@ -3,6 +3,7 @@
     A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 */
 
+// Recursive DFS
 var maxDepth = function(root) {
     if (root === null) return 0
         
@@ -10,4 +11,29 @@ var maxDepth = function(root) {
     let rightSubtree = 1 + maxDepth(root.right)
     
     return Math.max(leftSubtree, rightSubtree)
+}
+
+// Iterative BFS
+var maxDepth = function(root) {
+    if (root === null) return 0
+    
+    let queue = []
+    let depth = 0
+    queue.push(root)
+    
+    while (queue.length !== 0) {
+        let curr
+        let nodesInQ = queue.length
+        for (let i = 0; i < nodesInQ; i++) {
+            curr = queue.shift()
+            if (curr.left !== null) {
+                queue.push(curr.left)
+            }
+            if (curr.right !== null) {
+                queue.push(curr.right)
+            }
+        }
+        depth = depth + 1
+    }
+    return depth
 }
