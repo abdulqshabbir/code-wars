6dc9dfa076da5adfbe7764b78febd843042af842
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-04-19 07:47:10 +0800

    completed lowestCommonAncestor

--- leetcode/235_lowestCommonAncestor.js
+++ leetcode/235_lowestCommonAncestor.js
@@ -1,40 +1,13 @@
 var lowestCommonAncestor = function(root, p, q) {
-    const pathToP = traverseAndReturnPath(root, p) //[2]
-    const pathToQ = traverseAndReturnPath(root, q) // [2, 1]
+    let curr = root
     
-    let lca
-    
-    let numIterations = Math.min(pathToP.length, pathToQ.length)
-    let shorterPath = pathToP.length < pathToQ.length ? pathToP : pathToQ
-    
-    for (let i = 0; i < numIterations; i++) {
-        if (pathToP[i] === pathToQ[i]) {
-            lca = shorterPath[i]
-            console.log(lca)
-        }
-    }
-    return lca
-};
-
-function traverseAndReturnPath(root, node) {
-    let current = root
-    let path = []
-    path.push(current.val)
-      
-    while (current.val !== node.val) {
-        if (node.val < current.val) {
-            // traverse left subtree
-            current = current.left
-            path.push(current.val)
-        } else if (node.val > current.val) {
-            // traverse right subtree
-            current = current.right 
-            path.push(current.val)
+    while (curr !== null && p !== null && q!== null) {
+        if (p.val < curr.val && q.val < curr.val) {
+            curr = curr.left 
+        } else if (p.val > curr.val && q.val > curr.val) {
+            curr = curr.right
         } else {
-           // found node 
-           break 
+            return curr
         }
     }
-    
-    return path
-}
+};
