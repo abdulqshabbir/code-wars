e7f4b24268a2224cd2789a8a07446521db4db959
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-05-31 08:34:26 +0800

    completed leetcode 572 isSubtree

--- leetcode/572_isSubtree.js
+++ leetcode/572_isSubtree.js
@@ -11,19 +11,32 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
- var isSubtree = function(root, subRoot) {
-    if (subRoot === null) return true
-    if (root === null) return false
-    if (isSameTree(root, subRoot)) return true
-    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
-};
 
-var isSameTree = function(root, subRoot) {
-    if (root === null && subRoot === null) {
+ var isSubtree = function(root, subRoot) {
+    let res = false
+    
+    function dfs(root) {
+        if (isSameTree(root, subRoot)) {
+            res = true
+        } else {
+            if (root) {
+                dfs(root.left, subRoot)
+                dfs(root.right, subRoot)
+            }
+        }
+    } 
+    
+    dfs(root)
+    
+    return res
+}
+
+function isSameTree(p, q) {
+    if (p === null && q === null) {
         return true
     }
-    else if (root === null || subRoot === null) {
+    if (!p || !q) {
         return false
     }
-    return root.val === subRoot.val && isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right)
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
