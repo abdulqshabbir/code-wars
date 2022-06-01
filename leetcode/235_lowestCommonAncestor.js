7b3537039ee50ec26af69bd4be65e324b3fa46eb
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-01 08:15:00 +0800

    completed leetcode 235 LCA

--- leetcode/235_lowestCommonAncestor.js
+++ leetcode/235_lowestCommonAncestor.js
@@ -1,13 +1,20 @@
 var lowestCommonAncestor = function(root, p, q) {
     let curr = root
     
-    while (curr !== null && p !== null && q!== null) {
-        if (p.val < curr.val && q.val < curr.val) {
-            curr = curr.left 
-        } else if (p.val > curr.val && q.val > curr.val) {
-            curr = curr.right
-        } else {
+    while (curr) {
+        let forkExists = (p.val < curr.val && q.val > curr.val) 
+                       ||(p.val > curr.val && q.val < curr.val)
+        
+        if (forkExists) {
             return curr
+        } else if (curr.val === p.val || curr.val === q.val) {
+            return curr
+        } else {
+            if (p.val < curr.val) {
+                curr = curr.left
+            } else {
+                curr = curr.right
+            }
         }
     }
 };
