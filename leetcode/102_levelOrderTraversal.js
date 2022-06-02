a7d3830ee23f5b0289fa1418ee4e2587befec76b
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-02 16:44:04 +0800

    completed leetcode 102

--- leetcode/102_levelOrderTraversal.js
+++ leetcode/102_levelOrderTraversal.js
@@ -1,39 +1,28 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
-    * @param {TreeNode} root
-    * @return {number[][]}
- */
- var levelOrder = function(root) {
-    if (root === null) return []
+var levelOrder = function(root) {
+    let res = []
+    let q = []
+    let idx = 0
     
-    let result = []
-    let queue = []
-    let level = 0
+    if (root) {
+        q.push(root)
+    }
     
-    queue.push(root) 
-    while (queue.length !== 0) {
-        let current
-        result[level] = []
-        let qLength = queue.length
-        for (let i = 0; i < qLength; i++)   {
-            current = queue.shift()
-            result[level].push(current.val)
-            if (current.left !== null) {
-                queue.push(current.left)
-            }
-            if (current.right !== null) {
-                queue.push(current.right)
-            }
+    while (q.length !== 0) {
+        let qLength = q.length 
+        res[idx] = []
+        for (let i = 0; i < qLength; i++) {
+            let curr = q.pop()
+            res[idx].push(curr.val) 
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
         }
-        level = level + 1 
-    } 
+        idx++
+    }
     
-    return result
+    return res
 };
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
