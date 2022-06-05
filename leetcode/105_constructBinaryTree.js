565e147b527ab3f9e4b6a4366a71251d781041fb
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-05 09:37:13 +0800

    completed leetcode 105 construct binary tree

---
+++ leetcode/105_constructBinaryTree.js
@@ -0,0 +1,30 @@
+var buildTree = function(preorder, inorder) {
+    if (preorder.length === 0 || inorder.length === 0) {
+        return null
+    }
+
+    let subtreeRoot = new TreeNode(preorder[0])
+    let m = inorder.findIndex(elem => elem === preorder[0])
+
+    let leftPreorder = preorder.slice(1, m + 1)
+    let rightPreorder = preorder.slice(m+1)
+
+    let leftInorder = inorder.slice(0, m)
+    let rightInorder = inorder.slice(m + 1)
+
+    subtreeRoot.left = buildTree(leftPreorder, leftInorder)
+    subtreeRoot.right = buildTree(rightPreorder, rightInorder)
+
+    return subtreeRoot 
+};
+
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+let preorder = [3,9,20,15,7]
+let inorder = [9,3,15,20,7]
+
+buildTree(preorder, inorder)
