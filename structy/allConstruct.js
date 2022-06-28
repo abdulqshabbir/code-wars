8f4a4bdbdf0d29784ddb808b2ee9516cfd402b46
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-28 09:48:37 +0800

    completed allConstruct without memoization

---
+++ structy/allConstruct.js
@@ -0,0 +1,23 @@
+function isPrefix(prefix, word) {
+    return word.indexOf(prefix) === 0
+}
+
+function allConstruct(target, wordBank) {
+    if (target === "") return [[]]
+
+    let combinations = []
+
+    for (let prefix of wordBank) {
+        if (isPrefix(prefix, target)) {
+            let suffix = target.slice(prefix.length)
+            let waysToMakeSuffix = allConstruct(suffix, wordBank) // [[ple, le], [le]]
+            combinations.push(...waysToMakeSuffix.map(way => [prefix, ...way]))
+        }
+    }
+
+    return combinations
+}
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
+console.log(allConstruct('aaaaaaaaaaaaaaaaaaaaaaaaaz', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa']))
