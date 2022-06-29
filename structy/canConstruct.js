7cdccb3c1abf5db143e76371ce8fc152ca5ed75f
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-29 10:34:04 +0800

    completed canConstruct using tabulation

--- structy/canConstruct.js
+++ structy/canConstruct.js
@@ -26,4 +26,25 @@ function canConstruct(word, dictionary) {
     return false
 }
 
-console.log(canConstruct("abcdef", ["a", "ab", "d", "ef", "d"]))
+function canConstructTabulated(target, dictionary) {
+    let table = new Array(target.length + 1).fill(false)
+
+    table[0] = true
+
+    for (let i = 0; i < target.length + 1; i++) {
+        if (table[i] === true) {
+            for (let prefix of dictionary) {
+                if (target.slice(i).indexOf(prefix) === 0) {
+                    const inBounds = i + prefix.length <= target.length
+                    if (inBounds) table[i + prefix.length] = true
+                }
+            }
+        }
+    }
+
+    return table[target.length]
+}
+
+console.log(canConstructTabulated("", ["a", "ab", "d", "ef", "d"])) // true
+console.log(canConstructTabulated("abcdef", ["a", "ab", "d", "ef", "d"])) // false
+console.log(canConstructTabulated("abcdef", ["a", "abc", "d", "ef", "d"])) // true
