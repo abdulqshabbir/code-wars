de2e73bfac75e39fef947dac4d6c0cae1bded1ff
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-29 10:10:35 +0800

    completed bestSum and howSum using tabulation

--- structy/bestSum.js
+++ structy/bestSum.js
@@ -22,7 +22,32 @@ function bestSum (target, nums, memo = new Map()) {
     return shortestSum
 }
 
-console.log(bestSum(7, [5, 3, 4, 7])) // [4, 3]
+function bestSumTabulated(target, nums) {
+    const table = new Array(target + 1).fill(null)
+    table[0] = []
+
+    for (let i = 0; i < target + 1; i++) {
+        if (table[i] !== null) {
+            for (let num of nums) {
+                let inBounds = i + num <= target
+
+                if (inBounds && table[i + num] === null) {
+                    table[i+num] = [...table[i], num]
+                }
+                else if (inBounds && table[i+num] !== null) {
+                    let newCombination = [...table[i], num]
+                    if (newCombination.length < table[i+num].length) {
+                        table[i + num] = newCombination
+                    }
+                }
+            }
+        }
+    }
+
+    return table[target]
+}
+
+console.log(bestSum(7, [5, 3, 4, 7])) // [7]
 console.log(bestSum(8, [2, 3, 5])) // [5, 3]
 console.log(bestSum(8, [1, 4, 5])) // [4, 4]
 console.log(bestSum(100, [1, 2, 5, 25])) // [25, 25]
--- structy/howSum.js
+++ structy/howSum.js
@@ -12,3 +12,26 @@ function howSum(target, nums) {
 
     return null
 }
+
+function howSumTabulated(target, nums) {
+    const table = new Array(target + 1).fill(null)
+    table[0] = []
+
+    for (let i = 0; i < target + 1; i++) {
+        if (table[i] !== null) {
+            for (let num of nums) {
+                let inBounds = i + num <= target
+                if (inBounds) {
+                    table[i + num] = [...table[i], num]
+                }
+            }
+        }
+    }
+
+    return table[target]
+}
+
+console.log(howSumTabulated(5, [2, 1]))    // [1, 2, 2]
+console.log(howSumTabulated(7, [4, 2, 6])) // null
+console.log(howSumTabulated(0, [1, 2]))    // []
+
