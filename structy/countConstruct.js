a4ce79bbf7a9a9e7c372ddabf0bd0134229ebbb7
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-30 08:30:12 +0800

    completed countConstruct tabulated

--- structy/countConstruct.js
+++ structy/countConstruct.js
@@ -28,3 +28,25 @@ function countConstruct(target, wordBank) {
     return numberOfWays
 }
 
+function countConstructTabulated(target, wordBank) {
+    let table = new Array(target.length + 1).fill(0)
+    table[0] = 1
+
+    for (let i = 0; i <= target.length; i++) {
+        if (table[i] > 0) {
+            for (let prefix of wordBank) {
+                if (target.slice(i).indexOf(prefix) === 0) {
+                    let inBounds = i + prefix.length <= target.length
+                    if (inBounds) table[i + prefix.length] += table[i]
+                }
+            }
+        }
+    }
+
+    return table[target.length]
+}
+
+
+console.log(countConstructTabulated("hello", ["hel", "lo", "h", "ello"])) // 2
+console.log(countConstructTabulated("", ["hel", "lo", "h", "ello"])) // 1
+console.log(countConstructTabulated("hello", ["hep", "lo", "h", "pllo"])) // 0
