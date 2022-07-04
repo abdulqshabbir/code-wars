4eb17831b9275e1beebbc8b1b508d7165779f1b4
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-04 13:40:19 +0800

    completed leetcode 91 decodeWays

--- leetcode/91_decodeWays.js
+++ leetcode/91_decodeWays.js
@@ -23,3 +23,37 @@ var numDecodings = function(s) {
     
     return table[0]
 };
+
+function validChar(char) {
+    return parseInt(char) < 10
+}
+
+function validPair(pair) {
+    return parseInt(pair) >= 10 && parseInt(pair) <= 26
+}
+
+var numDecodings = function(s) {
+    function numWaysRecurse(i, memo) {
+        if (memo.has(i)) return memo.get(i) 
+        
+        if (s[i] === "0") return 0
+        
+        let ways = 0
+        
+        if (i+1 <= s.length && validChar(s[i])) {
+            ways += numWaysRecurse(i+1, memo)
+        }
+        if (i+2 <= s.length && validPair(s[i] + s[i+1])) {
+            ways += numWaysRecurse(i+2, memo)
+        }
+        
+        memo.set(i, ways)
+        
+        return ways
+    } 
+    
+    let memo = new Map()
+    memo.set(s.length, 1)
+    
+    return numWaysRecurse(0, memo)
+};
