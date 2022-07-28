ec50ddf34e78da70967d871171f8343df9f5731b
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-28 16:30:08 +0800

    completed leetcode 238

--- leetcode/238_productExceptSelf.js
+++ leetcode/238_productExceptSelf.js
@@ -5,28 +5,28 @@
 */
 
 var productExceptSelf = function(nums) {
-    const pre = new Array(nums.length).fill(1)
-    const post = new Array(nums.length).fill(1)
-    const answer = []
-    post[nums.length -1] = 1
+    const pre = new Array(nums.length)
+    const post = new Array(nums.length)
     
-    let preProd = 1
-    for (let i = 1; i < pre.length; i++) {
-       preProd = preProd * nums[i - 1]
-       pre[i] = preProd
+    pre[0] = 1
+    for (let i = 1; i < nums.length; i++) {
+        pre[i] = pre[i-1] * nums[i-1]
     }
     
-    let postProd = 1
-    for (let i = post.length - 2; i >= 0; i--) {
-       postProd = postProd * nums[i + 1]
-       post[i] = postProd
+    post[nums.length - 1] = 1
+    for (let i = nums.length - 2; i >=0; i--) {
+        post[i] = post[i+1] * nums[i+1]
     }
+    
+    const res = new Array(nums.length)
     
     for (let i = 0; i < nums.length; i++) {
-        answer[i] = pre[i] * post[i]
+        res[i] = pre[i] * post[i]
     }
-    return answer
+    
+    return res
 };
+
 
 // alternate solution with O(1) space if we exclude the answer array
 var productExceptSelf = function(nums) {
