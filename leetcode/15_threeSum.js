59b6905109b600e4b1a142221be492106db36312
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-31 13:13:43 +0800

    completed leetcode 15 three sum

--- leetcode/15_threeSum.js
+++ leetcode/15_threeSum.js
@@ -1,25 +1,72 @@
+// time: O(n^2) because for each starting number in triplet we have to iterate over the remaining array to find the last two nums in triplet
+// space: O(n) because we're using a hashset to keep track of triplets added so we don't have duplicates
 var threeSum = function(nums) {
-    let triplets = new Set()
-    nums = nums.sort((a, b) => a-b)
-    let result = []
-    for (let i = 0; i <= nums.length - 3; i++) {
-        let j = i + 1
-        let k = nums.length - 1
-        while (j < k) {
-           let sum = nums[i] + nums[j] + nums[k]
-           if (sum === 0 ) {
-                let tripMatch = [nums[i], nums[j], nums[k]]
-                if (!triplets.has(JSON.stringify(tripMatch))) {
-                    result.push(tripMatch)
-                    triplets.add(JSON.stringify(tripMatch))
+    nums.sort((a, b) => a - b)
+    const res = []
+    const set = new Set()
+    
+    for (let idx = 0; idx < nums.length - 2; idx++) {
+        const target = -nums[idx]
+        
+        let l = idx + 1
+        let r = nums.length -1
+        
+        while (l < r) {
+            const sum = nums[l] + nums[r]
+            if (sum === target) {
+                let key = nums[idx] + "," + nums[l] + "," + nums[r]
+                if (!set.has(key)) {
+                    res.push([nums[idx], nums[l], nums[r]])
+                    set.add(key)
                 }
-                j++
-           } else if (sum > 0) {
-                k-- 
-           } else {
-                j++
-           }
+                l++
+            } 
+            else if (sum > target) {
+                r-- 
+            }
+            else if (sum < target) {
+                l++
+            }
         }
     }
-    return result
+    return res
+};
+
+// O(n) space solution
+// similar to solution above but since array is sorted we can skip over duplicates
+// by intelligently moving pointers instead of keeping a hashset of tripletss
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b)
+    const res = []
+    for (let idx = 0; idx < nums.length - 2; idx++) {
+        const target = -nums[idx]
+        
+        if (idx > 0 && nums[idx] === nums[idx-1]) continue
+        
+        let l = idx + 1
+        let r = nums.length -1
+        
+        while (l < r) {
+            const sum = nums[l] + nums[r]
+            if (sum === target) {
+                res.push([nums[idx], nums[l], nums[r]])
+                l++
+                
+                while (l < r && nums[l] === nums[l-1]) {
+                    l++
+                }
+                
+                while (l < r && nums[r-1] === nums[r]) {
+                    r--
+                }
+            } 
+            else if (sum > target) {
+                r-- 
+            }
+            else if (sum < target) {
+                l++
+            }
+        }
+    }
+    return res
 };
