67dc7d06af272d864ccbc1278222cd7a4f59123a
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-01 18:54:35 +0800

    completed leetcode 11

--- leetcode/11_containsMostWater.js
+++ leetcode/11_containsMostWater.js
@@ -1,21 +1,18 @@
 var maxArea = function(height) {
-    let l = 0, r = height.length -1
-    let maxArea = 0
+    let maxArea = -Infinity
+    
+    let l = 0
+    let r = height.length - 1
     
     while (l < r) {
-        let area = (r - l) * Math.min(height[l], height[r])
+        const area = (r - l) * Math.min(height[l], height[r])
+        
         if (area > maxArea) maxArea = area
         
         if (height[l] < height[r]) {
             l++
-        } else if (height[r] < height[l]) {
-            r--
         } else {
-            if (height[l+1] > height[r-1]) {
-                l++
-            } else {
-                r--
-            }
+            r--
         }
     }
     return maxArea
