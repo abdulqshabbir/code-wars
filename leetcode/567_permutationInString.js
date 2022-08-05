08f267f718d7ec5bdcbef406472d0138acaa52e3
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-05 12:39:49 +0800

    completed leetcode 567

--- leetcode/567_permutationInString.js
+++ leetcode/567_permutationInString.js
@@ -1,53 +1,50 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
 var checkInclusion = function(s1, s2) {
     if (s1.length > s2.length) return false
+    
+    let s1Map = createFreqMap(s1, s1.length)
+    let s2Map = createFreqMap(s2, s1.length)
+    
     let l = 0
-    let r = 0
-    let counts = generateMap(s1) 
+    let r = s1.length - 1
+    
     while (r < s2.length) {
-        if (checkPermutation(counts)) {
-            return true
+        if (isPerm(s1Map, s2Map)) return true
+        
+        s2Map[s2[l]]--
+        l++ 
+        
+        if (r + 1 < s2.length) {
+            if (!(s2Map[s2[r+1]])) s2Map[s2[r+1]] = 0
+            s2Map[s2[r+1]]++
         }
-        if (!(s2[r] in counts)) {
-            r++
-            l = r
-            counts = generateMap(s1)
-            continue
-        }
-        if (counts[s2[r]] > 0) {
-            counts[s2[r]] = counts[s2[r]] - 1
-            r++
-        }
-        else {
-            counts[s2[r]] = counts[s2[r]] - 1
-            while (counts[s2[r]] < 0) {
-                counts[s2[l]] = counts[s2[l]] + 1
-                l++
-            }
-            r++
-        }
+        r++ 
     }
     
-    return checkPermutation(counts)
+    return false
 };
 
-function checkPermutation(counts) {
-    for (let [letter, count] of Object.entries(counts)) {
-        if (count !== 0) return false 
+function isPerm(map1, map2) {
+    for (let char1 in map1) {
+        if (!(char1 in map2)) return false
+        if (map1[char1] !== map2[char1]) return false
     }
     
     return true
 }
 
-function generateMap(str) {
-    let map = {}
+function createFreqMap(str, len) {
+     let freqMap = {}
+     
+     for (let i = 0; i < len; i++) {
+         if (!(str[i] in freqMap)) freqMap[str[i]] = 0
+         
+         freqMap[str[i]]++
+     }
     
-    for (let char of str) {
-        if (char in map) {
-            map[char]++
-        } else {
-            map[char] = 1
-        }
-    }
-    
-    return map
+    return freqMap
 }
